cdcd2ee974a59902bdaabac0b2e6a8db64b1fd38
Xiang Chen <xchen@foqum.io>
2020-08-19 16:14:40 +0200

    v7.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
@@ -11,22 +11,23 @@ def find_stubs(package):
     stubs = []
     for root, dirs, files in os.walk(package):
         for file in files:
-            path = os.path.join(root, file).replace(package + os.sep, '', 1)
+            path = os.path.join(root, file).replace(package + os.sep, "", 1)
             stubs.append(path)
     return {package: stubs}
+
 
 # python setup.py sdist
 # twine upload --skip-existing --repository pypi dist/*
 setup(
-    name='ortools-stubs',
+    name="ortools-stubs",
     maintainer="Xiang Chen",
     maintainer_email="xiangchenchen96@gmail.com",
     description="PEP 561 type stubs for Google OR-Tools",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/stradivari96/ortools-stubs",
-    license='MIT',
-    version="7.7.0",
-    packages=['ortools-stubs'],
-    package_data=find_stubs('ortools-stubs'),
+    license="MIT",
+    version="7.8.0",
+    packages=["ortools-stubs"],
+    package_data=find_stubs("ortools-stubs"),
 )
